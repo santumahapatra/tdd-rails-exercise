b9e753fe014ba300f2a0c5db243d61016efb2232
Santu Mahapatra <santu.m@multunus.com>
2014-11-19 15:25:46 +0530

    Adding threads to the controller search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,15 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    sleep(10)
+    new_thread = Thread.new{ go_look_for_stuff }
+    new_thread.join
   end
+
+  private
+    def go_look_for_stuff
+      puts Time.now
+      sleep(3)
+      puts Time.now
+      puts "I am supposed to be doing search but I make people wait and output to console"
+    end
 end
